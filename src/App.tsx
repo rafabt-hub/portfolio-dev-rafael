b9eedc5fbefcc8da58053db4bd6f807eafec4b26
Rafael <rafaelbthomaz@gmail.com>
2025-05-16 14:59:01 -0300

    adding projects component + media querys

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Job } from "./components/job"
+import { Projects } from "./components/projects"
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <div className={styles.container}>
         <Header />
         <Job />
+        <Projects />
       </div>
     </main>
   )
---
+++ src/components/projects/index.tsx
@@ -0,0 +1,62 @@
+import projectOne from "../../assets/images/Thumbnail_Project-01.png"
+import projectTwo from "../../assets/images/Thumbnail_Project-02.png"
+import projectTree from "../../assets/images/Thumbnail_Project-03.png"
+import projectFour from "../../assets/images/Thumbnail_Project-04.png"
+import projectFive from "../../assets/images/Thumbnail_Project-05.png"
+import projectSix from "../../assets/images/Thumbnail_Project-06.png"
+
+import styles from "./styles.module.css"
+
+export function Projects() {
+  return (
+    <div className={styles.projects}>
+      <div className={styles.backgroundBox}>
+       <img src={projectOne} className={styles.imgProject} alt="imagem projeto 1" />
+        <h3>Travelgram</h3>
+        <h4>
+        Rede social onde as pessoas mostram os registros de suas viagens pelo mundo 
+        </h4>
+      </div>
+
+      <div className={styles.backgroundBox}>
+       <img src={projectTwo} className={styles.imgProject} alt="imagem projeto 1" />
+        <h3>Tech News</h3>
+        <h4>
+        Homepage de um portal de noticias sobre a area de tecnologia 
+        </h4>
+      </div>
+
+      <div className={styles.backgroundBox}>
+       <img src={projectTree} className={styles.imgProject} alt="imagem projeto 1" />
+        <h3>Página de Receita</h3>
+        <h4>
+        Página com passo a passo de uma receita para cupcakes 
+        </h4>
+      </div>
+
+      <div className={styles.backgroundBox}>
+       <img src={projectFour} className={styles.imgProject} alt="imagem projeto 1" />
+        <h3>Zingen</h3>
+        <h4>
+        Landing Page completa e responsiva de um aplicativo de Karaokê 
+        </h4>
+      </div>
+
+      <div className={styles.backgroundBox}>
+       <img src={projectFive} className={styles.imgProject} alt="imagem projeto 1" />
+        <h3>Refund</h3>
+        <h4>
+        Um sistema para pedido e acompanhamento de reembolso 
+        </h4>
+      </div>
+
+      <div className={styles.backgroundBox}>
+       <img src={projectSix} className={styles.imgProject} alt="imagem projeto 1" />
+        <h3>Página de turismo</h3>
+        <h4>
+        Página com as principais informações para quem quer viajar para Busan 
+        </h4>
+      </div>
+    </div>
+  )
+}
